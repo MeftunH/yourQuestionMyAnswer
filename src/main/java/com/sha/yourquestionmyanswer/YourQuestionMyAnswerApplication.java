c718507801b1030d74ce55fe6e9b197d97d9b4f2
Maftun Hashimli <maftunhashimli@gmail.com>
2022-07-03 01:59:47 +0300

    Initial commit

---
+++ src/main/java/com/sha/yourquestionmyanswer/YourQuestionMyAnswerApplication.java
@@ -0,0 +1,13 @@
+package com.sha.yourquestionmyanswer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YourQuestionMyAnswerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YourQuestionMyAnswerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sha/yourquestionmyanswer/YourQuestionMyAnswerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sha.yourquestionmyanswer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YourQuestionMyAnswerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

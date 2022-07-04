bcd3cc0673649396aa40c9327689fce78603abff
Maftun Hashimli <maftunhashimli@gmail.com>
2022-07-04 05:19:15 +0300

    exclude id generation strategy

--- src/main/java/com/sha/yourquestionmyanswer/entities/User.java
+++ src/main/java/com/sha/yourquestionmyanswer/entities/User.java
@@ -2,16 +2,14 @@ package com.sha.yourquestionmyanswer.entities;
 
 import lombok.Data;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Data
 @Table(name = "users")
 public class User {
     @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
     @Column(name = "id", nullable = false)
     private Long id;
 

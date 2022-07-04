f227bdb097405f952da194607467c9e4ea47ae27
Maftun Hashimli <maftunhashimli@gmail.com>
2022-07-04 04:27:10 +0300

    set like repository class

---
+++ src/main/java/com/sha/yourquestionmyanswer/repos/LikeRepository.java
@@ -0,0 +1,8 @@
+package com.sha.yourquestionmyanswer.repos;
+
+import com.sha.yourquestionmyanswer.entities.Like;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LikeRepository extends JpaRepository<Like, Long> {
+
+}

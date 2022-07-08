02978a8c09e92b90c699b52b452dff91adcc5870
Maftun Hashimli <maftunhashimli@gmail.com>
2022-07-08 23:32:02 +0300

    set comment mappings

--- src/main/java/com/sha/yourquestionmyanswer/entities/Comment.java
+++ src/main/java/com/sha/yourquestionmyanswer/entities/Comment.java
@@ -1,6 +1,9 @@
 package com.sha.yourquestionmyanswer.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
 
 import javax.persistence.*;
 
@@ -13,11 +16,17 @@ public class Comment {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long id;
 
-    @Column(name = "user_id", nullable = false)
-    Long userId;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="user_id", nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JsonIgnore
+    User user;
 
-    @Column(name = "post_id", nullable = false)
-    Long postId;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="post_id", nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JsonIgnore
+    Post post;
 
     @Lob
     @Column(columnDefinition = "text", nullable = false)

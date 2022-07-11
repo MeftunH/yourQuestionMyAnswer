1a982aaa476a26c611df253122e98fa37d92bf31
Maftun Hashimli <maftunhashimli@gmail.com>
2022-07-12 02:20:40 +0300

    set jpa functions

--- src/main/java/com/sha/yourquestionmyanswer/repos/LikeRepository.java
+++ src/main/java/com/sha/yourquestionmyanswer/repos/LikeRepository.java
@@ -3,6 +3,13 @@ package com.sha.yourquestionmyanswer.repos;
 import com.sha.yourquestionmyanswer.entities.Like;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface LikeRepository extends JpaRepository<Like, Long> {
 
+    List<Like> findByUserIdAndPostId(Long aLong, Long aLong1);
+
+    List<Like> findByUserId(Long aLong);
+
+    List<Like> findByPostId(Long aLong);
 }

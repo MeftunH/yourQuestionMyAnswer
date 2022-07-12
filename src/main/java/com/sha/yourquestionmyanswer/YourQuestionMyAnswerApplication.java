25a448e1cc054d9a30b938ea45c04283ad07c909
Maftun Hashimli <maftunhashimli@gmail.com>
2022-07-12 03:17:51 +0300

    set cors settings

--- src/main/java/com/sha/yourquestionmyanswer/YourQuestionMyAnswerApplication.java
+++ src/main/java/com/sha/yourquestionmyanswer/YourQuestionMyAnswerApplication.java
@@ -3,12 +3,30 @@ package com.sha.yourquestionmyanswer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class YourQuestionMyAnswerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(YourQuestionMyAnswerApplication.class, args);
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("*"));
+        configuration.setAllowCredentials(true);
+        configuration.setAllowedHeaders(Arrays.asList("Access-Control-Allow-Headers","Access-Control-Allow-Origin","Access-Control-Request-Method", "Access-Control-Request-Headers","Origin","Cache-Control", "Content-Type", "Authorization"));
+        configuration.setAllowedMethods(Arrays.asList("DELETE", "GET", "POST", "PATCH", "PUT"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 
 }

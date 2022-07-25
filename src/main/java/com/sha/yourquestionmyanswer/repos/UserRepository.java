b0d015c094f831511cc208d5046d007e3480c14e
Maftun Hashimli <maftunhashimli@gmail.com>
2022-07-25 23:33:10 +0300

    set find by username functionality on user repository

--- src/main/java/com/sha/yourquestionmyanswer/repos/UserRepository.java
+++ src/main/java/com/sha/yourquestionmyanswer/repos/UserRepository.java
@@ -4,5 +4,5 @@ import com.sha.yourquestionmyanswer.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User,Long> {
-
+   User findByUsername(String username);
 }

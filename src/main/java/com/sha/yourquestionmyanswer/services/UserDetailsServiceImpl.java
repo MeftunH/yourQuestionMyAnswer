5e12b79c7416d1537f5e0ca4cd88d66daf7e9c54
Maftun Hashimli <maftunhashimli@gmail.com>
2022-07-25 23:34:37 +0300

    set load user by username functionality

---
+++ src/main/java/com/sha/yourquestionmyanswer/services/UserDetailsServiceImpl.java
@@ -0,0 +1,22 @@
+package com.sha.yourquestionmyanswer.services;
+
+import com.sha.yourquestionmyanswer.entities.User;
+import com.sha.yourquestionmyanswer.repos.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private UserRepository userRepository;
+
+    public UserDetailsServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username);
+        return null;
+    }
+}

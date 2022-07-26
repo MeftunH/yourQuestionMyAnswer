239749c9d4613d92b76dd1ef3f1afee004deed64
Maftun Hashimli <maftunhashimli@gmail.com>
2022-07-27 02:00:26 +0300

    set password encoding

--- src/main/java/com/sha/yourquestionmyanswer/controllers/AuthController.java
+++ src/main/java/com/sha/yourquestionmyanswer/controllers/AuthController.java
@@ -10,6 +10,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,6 +24,15 @@ public class AuthController {
     private JwtTokenProvider jwtTokenProvider;
 
     private UserService userService;
+
+    private PasswordEncoder passwordEncoder;
+
+    public AuthController(AuthenticationManager authenticationManager, JwtTokenProvider jwtTokenProvider, UserService userService, PasswordEncoder passwordEncoder) {
+        this.authenticationManager = authenticationManager;
+        this.jwtTokenProvider = jwtTokenProvider;
+        this.userService = userService;
+        this.passwordEncoder = passwordEncoder;
+    }
 
     @PostMapping("/login")
     public String login(@RequestBody UserRequest userRequest) {
@@ -40,7 +50,7 @@ public class AuthController {
         }
         User user = new User();
         user.setUsername(userRequest.getUsername());
-        user.setPassword(userRequest.getPassword());
+        user.setPassword(passwordEncoder.encode(userRequest.getPassword()));
         userService.save(user);
         return new ResponseEntity<>("User registered", HttpStatus.OK);
     }
